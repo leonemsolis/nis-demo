5c8e18601a3943d394956bb0f76947177bac6257
Ruslan <leonemsolis@gmail.com>
2020-12-03 23:51:05 +0600

    Add simple room / Add controller prototypes

---
+++ Assets/Scripts/MaterialManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour
+{
+    public Material glass;
+    public Material glassHighlight;
+
+    public static MaterialManager Instance{set; get;}
+
+    private void Awake() {
+        if(Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+
+            glass = Resources.Load<Material>("Glass");
+            glassHighlight = Resources.Load<Material>("GlassHighlight");
+            print(glass);
+            print(glassHighlight);
+
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PowderCanController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowderCanController : MonoBehaviour
+{
+    private Renderer rend;
+
+    private void Start() {
+        rend = GetComponent<Renderer>();
+    }
+
+    private void OnMouseDown() {
+        transform.position += Vector3.down * .03f;
+    }
+
+    private void OnMouseEnter() {
+        rend.material = MaterialManager.Instance.glassHighlight; 
+    }
+
+    private void OnMouseExit() {
+        rend.material = MaterialManager.Instance.glass;
+    }
+}
